1befd9dce2f9dd7c4f3e9c0ecf3cea73611f3476
MateMurcielago <galaxyuslol@gmail.com>
2024-05-29 16:10:46 -0300

    Calcular vigencia

--- src/main/java/utn/metodos_agiles/Main.java
+++ src/main/java/utn/metodos_agiles/Main.java
@@ -1,5 +1,7 @@
 package utn.metodos_agiles;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
@@ -7,5 +9,25 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+    }
+    
+    private static Date calcularVigencia(int edad, int nroLicTit) {
+    	Date vig = new Date();
+    	int mas = 0;
+    	if(edad < 21) {
+    		if(nroLicTit == 1) {
+    			mas = 1;
+    		} else mas = 3;
+    	} else if(edad <= 46) {
+    		mas = 5;
+    	} else if(edad <= 60) {
+    		mas = 4;
+    	} else if(edad <= 70) {
+    		mas = 3;
+    	} else if(edad > 70) {
+    		mas = 1;
+    	}
+    	vig.setYear(vig.getYear() + mas);
+    	return vig;
     }
 }
