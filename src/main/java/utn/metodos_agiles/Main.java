4c8e261ebe2f55d6933a8c4e3835598718939967
martinweidmann <martinweidmann29@gmail.com>
2024-06-08 13:59:53 -0300

    interfaces hechas

--- src/main/java/utn/metodos_agiles/Main.java
+++ src/main/java/utn/metodos_agiles/Main.java
@@ -4,13 +4,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client();
+      
+    	
+    	MenuPrincipal interfaz= new MenuPrincipal();
+    	interfaz.setVisible(true);
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	/*
+    	Client client = new Client();
         client.name = "Julian Sanchez";
         client.address = "Francia 2139";
         client.dni = "40121348";
 
         List<Item> items = List.of(new Item("Licencia A1", 350.5F));
 
-        FacturaGenerator.generar(client, items, "document.pdf");
+        FacturaGenerator.generar(client, items, "document.pdf");*/
     }
 }
